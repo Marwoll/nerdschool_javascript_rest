4c8755c2c72b0fff6989e81504df9ffd459d4535
Sivert Stavland <stavland.sivert@gmail.com>
2019-10-01 20:05:18 +0200

    got to 2.5 in the tutorial. Need fixing

--- tvShow/tvShowRouter.js
+++ tvShow/tvShowRouter.js
@@ -3,6 +3,12 @@ const TvShow = require('./TvShow');
 const tvShowService = require('./tvShowService');
 const tvShowRouter = express.Router();
 
+app.route('/tvshow/:tvShowId').get((req, res) => {
+    const tvShowId = req.params.tvShowId; 
+    tvShowService.getById(tvShowId);
+    res.send(`Fetching TV Show with id ${tvShowId}`);
+  });
+
 tvShowRouter.get('/', (req, res) => {
     res.json(tvShowService.getAll());
 });
--- tvShow/tvShowService.js
+++ tvShow/tvShowService.js
@@ -1,10 +1,25 @@
+const TvShow = require('./TvShow');
+
 class TvShowService {
     constructor() {
-        this.tvShows = [];
+        this.tvShows = [
+            new TvShow(1, 'Mr.Robot', 'Drama'), 
+            new TvShow(2, 'Black Mirror', 'Drama'),
+            new TvShow(3, 'Its always sunny', 'comedy')];
     }
 
     getAll() {
         return this.tvShows;
+    }
+
+    getById(tvShowId){
+        var showid;
+        for (i = 0; i < this.tvShows.length; i++) {
+            if (this.tvShows[i].id == tvShowId) {
+                showid = this.tvShows[i].id;
+            }
+        }
+        return showid;
     }
 }
 module.exports = new TvShowService();
